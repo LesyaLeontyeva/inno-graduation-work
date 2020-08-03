0cca357d87c3fbc40b572543ad429cbbf5039370
orchi <orchid345617@gmail.com>
2020-08-03 21:14:36 +0300

    неудачная попытка запускать в headless режиме тесты, пока что не работает. Закомментировала все строки связанные с headless. CI пока красный

--- conftest.py
+++ conftest.py
@@ -3,9 +3,11 @@ import pytest
 from pages.application import Application
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def app():
-    base_url = 'https://idemo.bspb.ru/'
+    base_url = "https://idemo.bspb.ru/"
+    # headless = request.config.getoption("--headless")
+    # fixture = Application(base_url, headless)
     fixture = Application(base_url)
     fixture.wd.implicitly_wait(10)
     fixture.wd.maximize_window()
--- pages/application.py
+++ pages/application.py
@@ -6,8 +6,12 @@ from pages.auth_page import LoginPage
 
 
 class Application:
+    # def __init__(self, base_url: str, headless):
     def __init__(self, base_url: str):
         driver_path = ChromeDriverManager().install()
+        # options: Options = Options()
+        # options.headless = headless
+        # self.wd = webdriver.Chrome(driver_path, options=options)
         self.wd = webdriver.Chrome(driver_path)
         self.base_url = base_url
         self.login = LoginPage(self)
